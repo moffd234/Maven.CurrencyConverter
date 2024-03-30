6919d536a3f1e5bcbe746dbea7de818e236a919a
moffd234 <moffd234@gmail.com>
2024-03-30 18:55:49 -0400

    Wrote comment explaining conversion equation

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -1,7 +1,15 @@
 package io.zipcoder.currencyconverterapplication;
 
 public class CurrencyConverter {
-    public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
-        return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
+    /*
+    (Conversion rate of A to UC) * (Conversion rate of Universal Currency to B) = conversion
+    i.e. 1.88 (euro) * 1(UC) / 2(USD) = X
+    or   1.88 (euro) * 1(UC) / 2.7(AUS) = X
+     */
+    public static Double convert(Double amountOfBaseCurrency,
+                                 ConvertableCurrency sourceCurrencyType,
+                                 CurrencyType targetCurrencyType) {
+        return amountOfBaseCurrency *
+        (sourceCurrencyType.convert(targetCurrencyType));
     }
 }
